ccd07c861899fc8fb7e27236e83bd7ea7950b823
sseungjae <61439454+sseungjae@users.noreply.github.com>
2020-12-17 22:01:33 +0900

    door js
    js function at door

--- door.js
+++ door.js
@@ -1,10 +1,21 @@
 (function() {
     const stageElem = document.querySelector('.stage');
 
+    //현재 활성화된 아이템을 저장
+    let currentItem;
+
     function doorHandler(e) {
         const targetElem = e.target;
 
-        targetElem.parentNode.classList.add('door-opened');
+        const currentItem = document.querySelector('.door-opened');
+        if(currentItem){
+        currentItem.classList.remove('door-opened');
+        }
+
+        if(targetElem.classList.contains('door-body')){
+            targetElem.parentNode.classList.add('door-opened');
+            currentItem = targetElem.parentNode;
+        }
     }
 
     stageElem.addEventListener('click', doorHandler);
